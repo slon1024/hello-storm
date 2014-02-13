8cdb1a68d722721ce027e0a66170c65c8e857c10
Vladimir <vova@getbase.com>
2014-02-13 11:31:07 +0100

    emit message from spout

--- src/main/java/spouts/ItemGenerator.java
+++ src/main/java/spouts/ItemGenerator.java
@@ -6,18 +6,24 @@ import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichSpout;
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
+import backtype.storm.tuple.Values;
+import backtype.storm.tuple.Fields;
 
 public class ItemGenerator extends BaseRichSpout {
+  private SpoutOutputCollector _collector;
 
   @Override
   public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+    _collector = collector;
   }
 
   @Override
   public void nextTuple() {
+    _collector.emit(new Values("Hello storm"));
   }
 
   @Override
   public void declareOutputFields(OutputFieldsDeclarer declarer) {
+    declarer.declare(new Fields("secret-message"));
   }
 }
